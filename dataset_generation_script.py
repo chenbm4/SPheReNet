71087b22e737259f38b694afc907c5c4fe19c003
Brian Chen <chenbm4@gmail.com>
2023-12-04 18:46:21 -0500

    changed to use GT folder instead of separate input directory

--- dataset_generation_script.py
+++ dataset_generation_script.py
@@ -1,7 +1,7 @@
 import os
 
 # Paths to your directories
-input_dir = '300W_LP_INPUT'
+# input_dir = '300W_LP_INPUT'
 gt_dirs = {
     'AFW': 'AFW_GT',
     'IBUG': 'IBUG_GT',
@@ -9,9 +9,8 @@ gt_dirs = {
 }
 output_file = 'train_data_file.txt'
 
-def find_gt_file(input_file, dataset_name):
+def find_gt_file(base_name, dataset_name):
     """Finds the corresponding ground truth file for a given input file."""
-    base_name = os.path.splitext(input_file)[0]
     gt_dir = gt_dirs[dataset_name]
     if dataset_name in ['AFW', 'HELEN']:
         gt_path = os.path.join(gt_dir, base_name + '.npz')
@@ -21,13 +20,13 @@ def find_gt_file(input_file, dataset_name):
 
 def process_dataset(dataset_name):
     """Processes each dataset and writes to the output file."""
-    dataset_dir = os.path.join(input_dir, dataset_name)
+    # dataset_dir = os.path.join(input_dir, dataset_name)
     with open(output_file, 'a') as file:
-        for input_file in os.listdir(dataset_dir):
-            if input_file.endswith('.jpg'):
-                input_path = os.path.join(dataset_dir, input_file)
+        for input_file in os.listdir(gt_dirs[dataset_name]):
+            if input_file.endswith('_facymap.jpg'):
+                input_path = input_file.rsplit('_facymap.jpg', 1)[0]
                 gt_path = find_gt_file(input_file, dataset_name)
-                file.write(f'{input_path} {gt_path}\n')
+                file.write(f'{input_path}.jpg {gt_path}\n')
 
 # Process each dataset
 for dataset_name in gt_dirs.keys():
